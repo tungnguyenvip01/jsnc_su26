ab83aa371f53b9356ad66031d3f3e0cf440eec09
thanhtung143 <tungnguyengenz143@gmail.com>
2026-08-04 14:41:21 +0700

    lan2

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("javascript") ;
+console.log("javascript");
+//$ php/
+
+//let const
+
+let name = "boy";
+const age = 22;
+let monney = 100;
+
+monney = 50;
+
+const yearTung = 2004;
+yearTung = 2004;
